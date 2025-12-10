413b7e45c98291a743d7b5ee723e0f2b61069dbb
Dean B <deantburrows88@gmail.com>
2025-12-10 19:48:51 +0100

    Refactor package modules and update dependencies
    Removed type exports from several package modules in Packages/, simplifying them to direct require statements. Deleted the Ripple package and removed node_modules dependencies related to @rbxts-js, including boolean, collections, console, es7-types, instance-of, luau-polyfill, math, number, react, react-globals, safe-flags, shared, string, symbol-luau, and timers. Added new modules for ByteNet, Charm, CharmSync, React-charm, and React-roblox. Updated aftman.toml to use rojo@7.6.1. Updated wally.lock and wally.toml to reflect new and removed dependencies.

--- Packages/Trove.lua
+++ Packages/Trove.lua
@@ -1,5 +1,1 @@
-local REQUIRED_MODULE = require(script.Parent._Index["sleitnick_trove@1.5.1"]["trove"])
-export type Trove = REQUIRED_MODULE.Trove 
-export type Trackable =
-REQUIRED_MODULE.Trackable 
-return REQUIRED_MODULE
+return require(script.Parent._Index["sleitnick_trove@1.5.1"]["trove"])
